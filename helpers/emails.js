51a456a0722f4371d14d716179ca211e7ccad1c1
Edwinrhc <edwinrhc@gmail.com>
2023-01-16 09:26:32 -0500

    Actualización de Email

--- helpers/emails.js
+++ helpers/emails.js
@@ -22,7 +22,7 @@ const emailRegistro = async (datos) => {
             <p>Hola ${nombre}, comprueba tu cuenta en mypaw.com </p>
 
             <p>Tu cuenta ya esta lista, solo debes de confirmarla en el siguiente enlace:
-            <a href="https://mypaw-production.up.railway.app/auth/confirmar/${token}">Confirmar Cuenta</a></p>
+            <a href="${process.env.BACKEND_URL}/auth/confirmar/${token}">Confirmar Cuenta</a></p>
 
 
          
@@ -55,13 +55,13 @@ const emailOlvidePassword = async (datos) => {
               <p>Hola ${nombre}, has solicitado reestablece tu password en mypaw.com </p>
   
               <p>Sigue en el siguiente enlace para generar un password nuevo:
-              <a href="${process.env.BACKEND_URL}:${ process.env.PORT ?? 3000}/auth/olvide-password/${token}">Restablecer password</a></p>
+              <a href="${process.env.BACKEND_URL}/auth/olvide-password/${token}">Restablecer password</a></p>
 
               <p>Si tu no solicitaste el cambio de password, puedes ignorar el mensaje</p>
               `,
     });
   };
-
+  // ${process.env.BACKEND_URL}:${ process.env.PORT ?? 3000}
 export { 
     emailRegistro,
     emailOlvidePassword
